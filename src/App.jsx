5c0cae0609b64bd9e6854d56788b3ece9dfbd95a
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-05-17 10:14:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Timer from "./components/Timer.jsx";
+import Stopwatch from "./components/Stopwatch.jsx";
+
+function App() {
+  return (
+    <div>
+      <Timer />
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TimeDisplay.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TimeDisplay() {
+  return <div>TimeDisplay</div>;
+}
+
+export default TimeDisplay;
